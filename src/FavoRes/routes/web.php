1eb8c084942c77d6cbe91bdb4aa034ff097ee076
kouki8710 <kxpi413@gmail.com>
2020-12-30 20:32:21 +0900

    [add] 日本語化-timezone local指定

--- src/FavoRes/routes/web.php
+++ src/FavoRes/routes/web.php
@@ -16,6 +16,10 @@ use App\Http\Controllers\FavoResController;
 
 Route::get('/',[FavoResController::class,"index"]);
 
+Route::get('/welcome',function(){
+    return view("welcome");
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia\Inertia::render('Dashboard');
 })->name('dashboard');
